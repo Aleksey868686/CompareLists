290de117919d252fbdea185564c7af1a98d3ac05
avvakumov <avvakumov@tslog.ru>
2023-01-23 18:40:09 +0300

    Add project files.

---
+++ LinkedListPerformance/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+
+namespace LinkedListPerformance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\SkillFactory\\module3\\ConparePerformance_13_6_1\\Text1.txt";
+            string text = File.ReadAllText(path);
+
+            // Сохраняем символы-разделители в массив
+            char[] delimiters = new char[] { ' ', '\r', '\n' };
+
+            // разбиваем нашу строку текста, используя ранее перечисленные символы-разделители
+            var words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            //Делим полученный массив слов на трети и получаем индексы первой и второй третей.
+            int indexOfFirstThird = words.Length / 3;
+            int indexOfSecondThird = (int)((int)words.Length - 0.6 * (int)words.Length);
+
+            LinkedList<string> linkedList = new LinkedList<string>();
+            LinkedListNode<string> node;
+
+            //Формируем Linked List из первой и последней трети
+            for (int i = 0; i < indexOfFirstThird; i++)
+            {
+                linkedList.AddLast(words[i]);
+            }
+            for (int i = indexOfSecondThird; i < words.Length; i++)
+            {
+                linkedList.AddLast(words[i]);
+            }
+
+            //// Запустим таймер и вставляем элементы после первого элемнта связного списка
+            var watch = Stopwatch.StartNew();
+
+            // определяем узел примерно посередине массива 
+            var insertNode = linkedList.Find("Барон");
+
+            //вставлем элементы примерно посередине связанного списка
+            for (int i = indexOfFirstThird; i < indexOfSecondThird; i++)
+            {
+                linkedList.AddAfter(insertNode, words[i]);
+            }
+
+            Console.WriteLine($"Вставка в LinkedList<string>: {watch.Elapsed.TotalMilliseconds} мс");
+        }
+    }
+}
---
+++ ListPerformance/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+
+namespace ListPerformance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\SkillFactory\\module3\\ConparePerformance_13_6_1\\Text1.txt";
+            string text = File.ReadAllText(path);
+
+            // Сохраняем символы-разделители в массив
+            char[] delimiters = new char[] { ' ', '\r', '\n' };
+
+            // разбиваем нашу строку текста, используя ранее перечисленные символы-разделители
+            var words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            //Делим полученный массив слов на трети и получаем индексы первой и второй третей.
+            int indexOfFirstThird = words.Length / 3;
+            int indexOfSecondThird = (int)((int)words.Length - 0.6 * (int)words.Length);
+
+            List<string> lst = new List<string>();
+
+            //Формируем List из первой и последней трети
+            for (int i = 0; i < indexOfFirstThird; i++)
+            {
+                lst.Add(words[i]);
+            }
+            for (int i = indexOfSecondThird; i < words.Length; i++)
+            {
+                lst.Add(words[i]);
+            }
+            //// Запустим таймер и вствляем элементы после первой трети списка
+            var watch = Stopwatch.StartNew();
+            for (int i = indexOfFirstThird; i < indexOfSecondThird; i++)
+            {
+                lst.Insert(i, words[i]);
+            }
+
+            Console.WriteLine($"Вставка в List<string>: {watch.Elapsed.TotalMilliseconds} мс");
+        }
+    }
+}
